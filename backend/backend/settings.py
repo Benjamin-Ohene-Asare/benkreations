6bb8376c03a6ac14b2d18fd9a4ab2d3ae94df1e3
Benjamin <benjaminandmichaelproject@gmail.com>
2026-06-28 19:08:02 +0000

    Serve media files on Render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,6 +116,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
